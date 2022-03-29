29fbeb45a8cdd96d4f9544d1595637201ed01d25
VenkataS1 <34402426+VenkataS1@users.noreply.github.com>
2022-03-29 01:19:16 -0400

    Add files via upload

---
+++ War_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_GameApp.swift
+//  War-Game
+//
+//  Created by Thejesh Sanikommu on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct War_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
